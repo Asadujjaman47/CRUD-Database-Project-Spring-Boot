2c9c22093fce365fe25f95b0cc3f00b1cfa93d43
Asadujjaman <41225800+Asadujjaman47@users.noreply.github.com>
2023-05-14 15:03:32 +0600

    Build Get Employee By ID REST API

--- src/main/java/com/asad/springboot/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/asad/springboot/service/impl/EmployeeServiceImpl.java
@@ -1,11 +1,13 @@
 package com.asad.springboot.service.impl;
 
+import com.asad.springboot.exception.ResourceNotFoundException;
 import com.asad.springboot.model.Employee;
 import com.asad.springboot.repository.EmployeeRepository;
 import com.asad.springboot.service.EmployeeService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
@@ -24,5 +26,21 @@ public class EmployeeServiceImpl implements EmployeeService {
     @Override
     public List<Employee> getAllEmployees() {
         return employeeRepository.findAll();
+    }
+
+    @Override
+    public Employee getEmployeeById(long id) {
+
+//        Optional<Employee> employee = employeeRepository.findById(id);
+//
+//        if(employee.isPresent()) {
+//            return employee.get();
+//        } else {
+//            throw new ResourceNotFoundException("Employee", "Id", id);
+//        }
+
+        // lambda expression
+        return employeeRepository.findById(id).orElseThrow(() ->
+                new ResourceNotFoundException("Employee", "Id", id));
     }
 }
