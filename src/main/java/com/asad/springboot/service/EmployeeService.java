db0435c0a2ccf8ca740bfc786020293fcb93276c
Asadujjaman <41225800+Asadujjaman47@users.noreply.github.com>
2023-05-14 13:23:18 +0600

    Build Create Employee REST API

---
+++ src/main/java/com/asad/springboot/service/EmployeeService.java
@@ -0,0 +1,7 @@
+package com.asad.springboot.service;
+
+import com.asad.springboot.model.Employee;
+
+public interface EmployeeService {
+    Employee saveEmployee(Employee employee);
+}
---
+++ src/main/java/com/asad/springboot/service/impl/EmployeeServiceImpl.java
@@ -0,0 +1,21 @@
+package com.asad.springboot.service.impl;
+
+import com.asad.springboot.model.Employee;
+import com.asad.springboot.repository.EmployeeRepository;
+import com.asad.springboot.service.EmployeeService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService {
+
+    private EmployeeRepository employeeRepository;
+
+    public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    @Override
+    public Employee saveEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+}
