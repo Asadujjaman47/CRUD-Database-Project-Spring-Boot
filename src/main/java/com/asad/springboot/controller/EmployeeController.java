3835242779e37e78c7c07684571c265e596820b8
Asadujjaman <41225800+Asadujjaman47@users.noreply.github.com>
2023-05-14 13:33:42 +0600

    Build Get All Employees REST API

--- src/main/java/com/asad/springboot/controller/EmployeeController.java
+++ src/main/java/com/asad/springboot/controller/EmployeeController.java
@@ -4,10 +4,9 @@ import com.asad.springboot.model.Employee;
 import com.asad.springboot.service.EmployeeService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/employees")
@@ -22,5 +21,11 @@ public class EmployeeController {
     @PostMapping()
     public ResponseEntity<Employee> saveEmployee(@RequestBody Employee employee) {
         return new ResponseEntity<>(employeeService.saveEmployee(employee), HttpStatus.CREATED);
+    }
+
+    // build get all employees REST API
+    @GetMapping()
+    public List<Employee> getAllEmployees() {
+        return employeeService.getAllEmployees();
     }
 }
